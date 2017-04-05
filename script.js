a55de349546654809094f48236b5557a510ed9eb
Donovan Kim-adams <dcadams6@asu.edu>
2017-04-04 18:39:27 -0700

    Add files via upload
    yeee

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    /**js for the index page**/
+        $("#name").hide();
+        $("#myPic").hide();
+        $("#name").fadeIn(4000);
+        $("#myPic").fadeIn(4000);
+        /** Js for the about page**/
+        $(".about").effect("slide",1000);
+        $(".about2").hide();
+        $(".about2").delay( 800 ).effect("slide",1000);
+        $(".skill").hide();
+        $( window ).scroll(function() {
+          $(".skill").fadeIn( 1000);
+          /****/
+
+});
+
+    });
